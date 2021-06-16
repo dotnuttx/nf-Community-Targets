f88627ac5f16df0d684f866ee3c6cae59bfcd931
Matheus Castello <matheus@castello.eng.br>
2021-06-15 23:01:06 -0300

    posix: build: Add deubg release switches
    To build a Debug binary use:
    
    ```
    ./build.sh debug
    ```
    
    Today a static linked binary size is:
    
    2.7MB Release
    20MB Debug
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- posix/build.sh
+++ posix/build.sh
@@ -5,8 +5,13 @@ clear
 # cleanup the build folder
 rm -rf build
 mkdir build
+cd build
 
 # build it
-cd build
-cmake ..
+if [ "$1" == "debug" ]; then
+    cmake -D CMAKE_BUILD_TYPE=Debug ..
+else
+    cmake -D CMAKE_BUILD_TYPE=Release ..
+fi
+
 make -j12
